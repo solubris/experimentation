189ff6a860c8cc17f2541b2d6061956376617efa
Tim Walters <tim@solubris.com>
2021-10-08 20:19:48 +0100

    prepend the jdk

--- src/main/sh/combine.sh
+++ src/main/sh/combine.sh
@@ -8,15 +8,21 @@ set -eo pipefail
 
 sp=$(dirname "$0")
 
+SED_OPTIONS=(-i -e)
+case "$(uname)" in
+Darwin*) SED_OPTIONS=(-i "" -e) ;;
+esac
+
 bench_files_dir=$1
 
 echo "combining files in $bench_files_dir"
 for bench_file in $bench_files_dir/**/*.txt; do
   echo "combining $bench_file into $bench_files_dir/all.txt"
-  bench_file_dir=${bench_file%/*}
-  bench_file_dir=${bench_file_dir#*/}
+  bench_file_dir=${bench_file%/*} # strip filename
+  bench_file_dir=${bench_file_dir##*/}  # strip parent dirs
   echo "bench_file_dir=$bench_file_dir"
 
+  sed -E "${SED_OPTIONS[@]}" "s/^/$bench_file_dir /" $bench_file
   cat $bench_file >> $bench_files_dir/all.txt
   rm -rf $bench_file
 done
