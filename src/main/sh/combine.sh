cb513623a31396dd2d98f29c6f6dc4b51a13fc9e
Tim Walters <tim@solubris.com>
2021-10-08 19:28:45 +0100

    combine artifact files 2

--- src/main/sh/combine.sh
+++ src/main/sh/combine.sh
@@ -10,7 +10,8 @@ sp=$(dirname "$0")
 
 bench_files_dir=$1
 
-for bench_file in $bench_files_dir/**/*.txt; do
+echo "combining files in $bench_files_dir"
+for bench_file in $bench_files_dir/**/*; do
   echo "combining $bench_file into $bench_files_dir/all.txt"
   cat $bench_file >> $bench_files_dir/all.txt
   rm -rf $bench_file
